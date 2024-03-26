cc1ec60f0bc25c61daaedc3143e9effe14917652
KhangSimple <you@example.com>
2024-03-26 23:48:04 +0700

    Kiem thu dong du lieu

--- src/PasswordTest.java
+++ src/PasswordTest.java
@@ -3,12 +3,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordTest {
     @org.junit.jupiter.api.Test
     void test1() throws Exception {
-        assertEquals(Password.passwordEvaluation(-1, false), "ValueError");
+        assertEquals(Password.passwordEvaluation(17, false), "ValueError");
     }
 
     @org.junit.jupiter.api.Test
     void test2() throws Exception {
-        assertEquals(Password.passwordEvaluation(5, false), "Very weak");
+        assertEquals(Password.passwordEvaluation(4, false), "Very weak");
     }
 
     @org.junit.jupiter.api.Test
@@ -24,16 +24,16 @@ public class PasswordTest {
 
     @org.junit.jupiter.api.Test
     void test5() throws Exception {
-        assertEquals(Password.passwordEvaluation(11, false), "Weak");
+        assertEquals(Password.passwordEvaluation(16, true), "Strong");
     }
 
     @org.junit.jupiter.api.Test
     void test6() throws Exception {
-        assertEquals(Password.passwordEvaluation(15, true), "Strong");
+        assertEquals(Password.passwordEvaluation(11, false), "Weak");
     }
 
     @org.junit.jupiter.api.Test
     void test7() throws Exception {
-        assertEquals(Password.passwordEvaluation(15, false), "Medium");
+        assertEquals(Password.passwordEvaluation(16, false), "Medium");
     }
 }
